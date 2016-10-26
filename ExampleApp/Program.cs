9ea4336a1aad4c2c1c9b16e05f406391af07aeb2
mchlek.b@gmail.com <mchlek.b@gmail.com>
2016-10-26 07:31:28 +0200

    init

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            var x = Console.ReadLine();
+            Console.WriteLine($"Hello, {x}!");
+        }
+    }
+}
